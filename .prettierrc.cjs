4499f19bf8925d35680a193a123f258f080003a8
Korazza <corattiroberto@gmail.com>
2023-11-14 13:38:57 +0100

    rename .prettierrc.json to .prettierrc.cjs

---
+++ .prettierrc.cjs
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+	arrowParens: "always",
+	bracketSpacing: true,
+	endOfLine: "lf",
+	htmlWhitespaceSensitivity: "css",
+	jsxSingleQuote: false,
+	printWidth: 80,
+	proseWrap: "preserve",
+	quoteProps: "as-needed",
+	semi: false,
+	singleAttributePerLine: false,
+	tabWidth: 2,
+	singleQuote: false,
+	trailingComma: "es5",
+	useTabs: true,
+	plugins: ["prettier-plugin-tailwindcss"],
+	tailwindFunctions: ["cn"],
+}
