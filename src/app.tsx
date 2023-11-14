5eff112d9355f3347bd94fdfa3695ff0b5afb490
Korazza <corattiroberto@gmail.com>
2023-11-14 13:22:06 +0100

    change "vh" units to "dvh" in all css

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,7 @@ import "@/styles/app.css"
 export const App = () => {
 	return (
 		<div className="flex w-full flex-col items-center space-y-8 py-3 md:py-0">
-			<div className="flex w-full flex-col items-center space-y-10 md:h-[calc(100vh-4rem)] md:max-w-screen-md md:space-y-8">
+			<div className="flex w-full flex-col items-center space-y-10 md:min-h-[calc(100dvh-4rem)] md:max-w-screen-md md:space-y-8">
 				<UpperControls />
 				<Bars />
 				<LowerControls />
